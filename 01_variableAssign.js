e5595dd5db20468397d0dcf060676f8be549a9b0
PradipBhusnar7 <pradipbhusnar7077@gmail.com>
2023-02-23 15:34:51 +0530

    01variableAssign
    01variableAssign

---
+++ 01_variableAssign.js
@@ -0,0 +1,22 @@
+// Assignment 01 
+console.log("==============step1)=============");
+var myFullName;
+console.log("myFullName");
+myFullName = "Pradip Bhusnar"
+console.log(myFullName);
+myFullName = "Pradip Bramhadev Bhusnar";
+console.log(myFullName);
+
+console.log("==============step2)=============");
+var myLove ="I love only Js"
+console.log(" myLove");
+
+console.log("==============step3)=============");
+var firstName = "Pradip";
+var lastName = "Bhusnar";
+var age = "22";
+var address = "Pandharpur";
+var pinCode = "413304";
+var gender = "Male";
+var isMarried = "No";
+console.log(firstName,lastName,age,address,pinCode,gender,isMarried);
