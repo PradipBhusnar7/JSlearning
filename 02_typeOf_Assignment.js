2c729f6208fd667aa2bedd0ca94dd32db7582042
PradipBhusnar7 <pradipbhusnar7077@gmail.com>
2023-02-25 23:28:51 +0530

    typeOf_Assignment
    typeOf_Assignment

---
+++ 02_typeOf_Assignment.js
@@ -0,0 +1,27 @@
+console.log("====Part1====");
+
+var bankName;
+bankName = "SBI Bank"
+console.log(bankName,typeof bankName);
+
+console.log("====Part2====");
+
+var marks = '90%';
+console.log(marks, typeof marks);
+
+console.log("====Part3====");
+
+var isWorking;
+isWorking = true;
+console.log(isWorking, typeof isWorking);
+
+
+console.log("====Part4====");
+
+var totalCount = "Hundred and Seven";
+console.log(totalCount , typeof totalCount);
+var totalCount = 107;
+console.log(totalCount , typeof totalCount);
+
+
+
