75752d80f47400a6367a2663d4421d33354ef4e9
PradipBhusnar7 <pradipbhusnar7077@gmail.com>
2023-02-25 23:28:17 +0530

    swapVariable_Assignment
    swapVariable_Assignment

---
+++ 02_swapVariables_Assignment.js
@@ -0,0 +1,17 @@
+console.log("==Part1==");
+var sweety = "Sweety";
+var cutie = "Cutie";
+var temp = sweety;
+sweety = cutie ;
+cutie = temp ;
+console.log(sweety,cutie);
+
+console.log("==Part2=="); 
+num1 = 100;
+num2 = 200;
+num3 = 300;
+var temp2 = num1;
+num1 = num2; 
+num2 = num3;
+num3 = temp2;
+console.log(num1,num2,num3);
