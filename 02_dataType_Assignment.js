e39affaa2a97720dc0920f3b2977f332d86f6ef8
PradipBhusnar7 <pradipbhusnar7077@gmail.com>
2023-02-25 23:27:38 +0530

    dataType_Assignment
    dataType_Assignment

---
+++ 02_dataType_Assignment.js
@@ -0,0 +1,24 @@
+console.log("====Part1====");
+
+var num1 = 07;
+var num2 = 97;
+var num3 = 85;
+console.log("Three numbers are",num1, num2, num3);
+console.log("Datatype of  three number of  is", typeof num1, typeof num2,typeof num3);
+
+var str1 = "string1st";
+var str2 = "string2nd";
+var str3 = "string3rd";
+console.log("Three strings are",str1 ,str2,str3);
+console.log("Datatype of three strings are",typeof str1 , typeof str2 , typeof str3);
+
+var html = true;
+var css = true;
+var js = true;
+console.log("Three boolean variables are", html,css,js);
+console.log("Datatype ofthree boolean variables are", typeof html, typeof css, typeof js);
+
+console.log("====Part2====");
+console.log("==Difference between null vs undefined==");
+console.log("null- Storing the null in to variable making that this variable is empty.");
+console.log("undefined- It means when we declare a variable and does not assign any value in that case variable holds one value ");
